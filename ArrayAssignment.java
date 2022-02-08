f1f62c3a2443d39d250fc71c42394535950f37b1
Evan <98685984+utopiatopia@users.noreply.github.com>
2022-02-08 20:14:01 +0000

    Update README.md and use Replit

---
+++ ArrayAssignment.java
@@ -0,0 +1,10 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class ArrayAssignment {
+  public static void main(String[] args) {
+    File schoolFile = new File("schools.txt");
+    ArrayData schoolData = new ArrayData();
+    
+  }
+}
